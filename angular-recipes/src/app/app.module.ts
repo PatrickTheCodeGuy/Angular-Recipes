b86905140810a02600e8a9360d8134956743434c
Patrick Thompson <patrick.jthompson93@gmail.com>
2019-03-02 16:10:59 -0800

    testing out nesting components and css styling

--- angular-recipes/src/app/app.module.ts
+++ angular-recipes/src/app/app.module.ts
@@ -1,11 +1,13 @@
+import { ServerComponent } from "./server/server.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
